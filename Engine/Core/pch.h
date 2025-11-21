09a946bd011dd54b3167edb92bbaf92a05bba720
nansu0425 <nansu0425@gmail.com>
2025-11-21 11:37:36 +0900

    refactor: Engine의 Pch가 ExcepAPI.h 포함하도록 변경

--- Engine/Core/pch.h
+++ Engine/Core/pch.h
@@ -1,22 +1,12 @@
 ﻿#pragma once
 
-// 엔진 기본 타입
-#include "Core/Types.h"
+// 엔진 공개 API
+#include "Core/ExcepAPI.h"
 
 // Windows 헤더
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-// C++ 표준 라이브러리
-#include <memory>
-#include <string>
-#include <vector>
-#include <algorithm>
-
 // DirectX 헤더
 #include <d3d11.h>
 #include <wrl/client.h>
-
-// 엔진 수학 라이브러리
-#include "Math/Vector3.h"
-#include "Math/Vector4.h"
